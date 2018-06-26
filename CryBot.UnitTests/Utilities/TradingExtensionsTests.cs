9d5a650e3beff576e9213b17cff133e94798c9d9
Bogdan Bujdea <bogdan@thewindev.net>
2018-06-26 23:54:29 +0300

    Added unit tests project

---
+++ CryBot.UnitTests/Utilities/TradingExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Bittrex.Net.Objects;
+using CryBot.Core.Utilities;
+using FluentAssertions;
+using Xunit;
+
+namespace CryBot.UnitTests.Utilities
+{
+    public class TradingExtensionsTests
+    {
+        [Fact]
+        public void ToCoinBalance_Should_Convert()
+        {
+            var bittrexBalance = new BittrexBalance();
+            bittrexBalance.Currency = "XLM";
+            bittrexBalance.Available = 100;
+            bittrexBalance.Balance = 100;
+            bittrexBalance.Pending = 0;
+            var coinBalance = bittrexBalance.ConvertToCoinBalance();
+            coinBalance.Currency.Should().Be("XLM");
+            coinBalance.MarketName.Should().Be("BTC-XLM");
+            coinBalance.Balance.Should().Be(100);
+        }
+    }
+}
